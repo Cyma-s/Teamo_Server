864cb2941bd75f39c3ccccbaa3ae1a4de5e344ea
jaehoon0 <choco730522@gmail.com>
2021-11-26 14:09:08 +0900

    edit(ignore)del migraion

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { Entity, Column, PrimaryColumn, OneToOne, JoinColumn } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryColumn()
+    id: string;
+
+	@Column()
+    password: string;
+
+	@Column()
+	name: string;
+
+	@Column()
+	email: string;
+
+	@Column()
+	department: string;
+
+	@Column()
+	std_num: string;
+
+	@Column({type: 'longtext', default: null})
+	introduction: string;
+}
