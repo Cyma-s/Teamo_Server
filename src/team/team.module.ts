0e4dbf203e31b76527913125cebab86b677c97bd
jaehoon0 <choco730522@gmail.com>
2021-12-04 19:48:59 +0900

    team

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from 'src/entities/member.entity';
+import { Message } from 'src/entities/message.entity';
+import { User } from 'src/entities/user.entity';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Member])],
+  imports: [TypeOrmModule.forFeature([Member, User, Message])],
   controllers: [TeamController],
   providers: [TeamService]
 })
